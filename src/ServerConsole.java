7c5d953acfa48c25f3cf54572199e2a310ac6d24
Nathantraineau <nathan.trn@orange.fr>
2018-11-20 05:41:02 +0100

    first commit

---
+++ src/ServerConsole.java
@@ -0,0 +1,130 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+
+//import EchoServer;
+
+import client.ChatClient;
+import common.ChatIF;
+
+public class ServerConsole implements ChatIF {
+	
+	EchoServer server;
+	
+	public ServerConsole(int port)
+	{
+		
+		try 
+		{
+			 server = new EchoServer(port,this);
+			accept();
+		} 
+		catch (Exception ex) 
+		{
+			System.out.println("ERROR - Could not listen for clients!");
+		}
+	}
+	//Class variables *************************************************
+	  
+	  /**
+	   * The default port to connect on.
+	   */
+	  final public static int DEFAULT_PORT = 5555;
+	  
+	  //Instance variables **********************************************
+	  
+	  /**
+	   * The instance of the client that created this ConsoleChat.
+	   */
+	  ChatIF serverUI; 
+	  
+	  //Constructors ****************************************************
+
+	  /**
+	   * Constructs an instance of the ClientConsole UI.
+	   * @param host The hostclient to connect to.
+	   * @param port The port to connect on.
+	 * @return 
+	   */
+
+	  public void display(String message) {
+			System.out.println(message);
+		}
+	  //Instance methods ************************************************
+	  
+	  /**
+	   * This method waits for input from the console.  Once it is 
+	   * received, it sends it to the client's message handler.
+	   */
+	  public void accept() 
+	  {
+	    try
+	    {
+	      BufferedReader fromConsole = 
+	        new BufferedReader(new InputStreamReader(System.in));
+	      String message;
+
+	      while (true) 
+	      {
+	        message = fromConsole.readLine();
+	        server.handleMessageFromServerUI(message);
+	      }
+	    } 
+	    catch (Exception ex) 
+	    {
+	      System.out.println
+	        ("Unexpected error while reading from console!");
+	    }
+	  }
+
+	  /**
+	   * This method overrides the method in the ChatIF interface.  It
+	   * displays a message onto the screen.
+	   *
+	   * @param message The string to be displayed.
+	   */
+
+	  
+	  //Class methods ***************************************************
+	  
+	  /**
+	   * This method is responsible for the creation of the Client UI.
+	   *
+	   * @param args[0] The host to connect to.
+	   */
+	  public static void main(String[] args) 
+	  {
+	    String host = "";
+	    int port = 0;  //The port number
+
+	    try
+	    {
+	      host = args[0];
+	    }
+	    catch(ArrayIndexOutOfBoundsException e)
+	    {
+	      host = "localhost";
+	    }
+	    
+
+	    try
+	    {
+	      port = Integer.parseInt(args[0]); //Get port from command line
+	    }
+	    catch(Throwable t)
+	    {
+	      port = EchoServer.DEFAULT_PORT; //Set port to 5555
+	    }
+		
+	    ServerConsole sc = new ServerConsole(port);
+	    
+	   
+	    
+	    
+	  }
+
+}
